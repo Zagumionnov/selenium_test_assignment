5fea854cba28becc2c5fe382e2e878d02f3e4c63
zagumionnov <zagumionnov6@gmail.com>
2021-12-08 23:23:15 +0200

    get bestbay price

--- tests/test_shopping.py
+++ tests/test_shopping.py
@@ -6,15 +6,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC # noqa
 from selenium.webdriver.common.keys import Keys
 
-product = 'OnePlus 8T'  # Samsung Galaxy S21 Ultra
-color = 'Green'
+product = 'OnePlus 8T'
+color = 'green'
 
 browser = webdriver.Chrome()
 
 
 def test_shopping(driver):
+    amazon_price = 0
+    bestbuy_price = 0
+
     try:
         driver.get('https://www.amazon.com/')
+
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((
             By.XPATH, "//input[@id='twotabsearchtextbox']"
         )))
@@ -54,7 +58,40 @@ def test_shopping(driver):
             By.XPATH, "//*[@id='corePrice_desktop']/div/table/tbody/tr/td[2]/span[1]/span[2]"
         ).text
         amazon_price = float(price.lstrip('$'))
-        print(amazon_price, type(amazon_price))
+
+        driver.get('https://www.bestbuy.com/')
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((
+            By.XPATH, "//a[2]/img"
+        )))
+        driver.find_element(By.XPATH, "//a[2]/img").click()
+
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((
+            By.XPATH, "//*[@id='widgets-view-email-modal-mount']/div/div/div[1]/div/div/div/div/button"
+        )))
+        driver.find_element(By.XPATH, "//*[@id='widgets-view-email-modal-mount']/div/div/div[1]/div/div/div/div/button").click()
+
+        time.sleep(5)
+        elem = driver.find_element(By.XPATH, "//*[@id='gh-search-input']")
+        elem.send_keys(product)
+        elem.send_keys(Keys.RETURN)
+
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((
+            By.XPATH, "//*[@id='main-filters']/div[3]/div[2]/section[8]"
+        )))
+        driver.find_element(
+                By.XPATH, f"//li/div/div/div/label/span[1]/a/span[contains(text(),'{color.capitalize()}')]"
+        ).click()
+
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((
+            By.XPATH, "//*[@id='sort-by-select']"
+        )))
+        driver.find_element(By.XPATH, "//*[@id='sort-by-select']").click()
+        driver.find_element(By.XPATH, "//*[@id='sort-by-select']/option[5]").click()
+
+        price = driver.find_element(
+                By.XPATH, "//div[@class='priceView-hero-price priceView-customer-price']/span[1]"
+            ).text
+        bestbuy_price = float(price.lstrip('$'))
 
     except Exception as ex:
         print(ex)
@@ -62,9 +99,8 @@ def test_shopping(driver):
         driver.close()
         driver.quit()
 
+    assert amazon_price > bestbuy_price
+
 
 if __name__ == '__main__':
     test_shopping(browser)
-
-
-    # assert amazon_price > bestbuy_price
